ae9a132816369a2b1ecaf75dc8c016626d554874
Ryca77 <ryan.carter77@gmail.com>
2016-10-28 15:20:45 +0100

    added props attributes

--- js/components/person-list.js
+++ js/components/person-list.js
@@ -3,18 +3,25 @@ var ReactDOM = require('react-dom');
 
 var Person = require('./person');
 
-var PersonList = React.createClass({
-	render: function() {
-		var people = [];
-		for(var i = 0; i < 5; i++) {
-			people.push(<Person />);
-		}
-		return (
-			<div className="person-list">
-				{people}
-			</div>
-		);
-	}
-});
+Person.defaultProps = {
+    imageUrl: 'http://www.gravatar.com/avatar/?d=identicon'
+};
+
+var PersonList = function() {
+	return (
+		<div className="person-list">
+			<Person	name='Derek Zoolander'
+    				imageUrl='http://www.leadersayswhat.com/wp-content/uploads/2016/02/derek-zoolander-thumb.jpg'
+    				job='Male model, bit of a numpty' />
+    		<Person	name='Donald Knuth'
+    				imageUrl='http://www-cs-faculty.stanford.edu/~uno/don.gif'
+    				job='Clever chap, did some stuff' />
+    		<Person	name='Ryan Carter'
+    				
+    				job='Learning React' />
+		</div>
+		
+	);
+};
 
 module.exports = PersonList;
--- js/components/person.js
+++ js/components/person.js
@@ -1,17 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Person = function() {
-    var name = 'Derek Zoolander';
-    var imageUrl = 'http://uifaces.com/assets/static/images/zoolander.jpg';
-    var job = 'Male model';
+var Person = function(props) {
     return (
         <div className="person">
-            <div className="person-name">{name}</div>
-            <img className="person-img" src={imageUrl} />
-            <div className="person-job">{job}</div>
+            <div className="person-name">{props.name}</div>
+            <img className="person-img" src={props.imageUrl} />
+            <div className="person-job">{props.job}</div>
         </div>
     );
 };
+
+
 
 module.exports = Person;
