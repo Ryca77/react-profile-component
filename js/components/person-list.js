bac0170c9e32ba200126395bb81ef492a2e15689
Ryca77 <ryan.carter77@gmail.com>
2016-10-28 09:49:39 +0100

    react tutorials

--- js/components/person-list.js
+++ js/components/person-list.js
@@ -1,15 +1,20 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
 var Person = require('./person');
 
-var PersonList = function() {
-	return (
-		<div className="person-list">
-			<Person />
-			<Person />
-			<Person />
-			>Person />
-			<Person />
-		</div>
-	);
-};
+var PersonList = React.createClass({
+	render: function() {
+		var people = [];
+		for(var i = 0; i < 5; i++) {
+			people.push(<Person />);
+		}
+		return (
+			<div className="person-list">
+				{people}
+			</div>
+		);
+	}
+});
 
 module.exports = PersonList;
--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 require('babel-polyfill');
 
+var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Person = require('./components/person');
+var PersonList = require('./components/person-list');
 
 document.addEventListener('DOMContentLoaded', function() {
 	ReactDOM.render(<PersonList />, document.getElementById('app'));
 });
-
